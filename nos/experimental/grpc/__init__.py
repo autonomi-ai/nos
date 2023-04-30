2a9bf8907b034a6bd048cfa140e7b8efb9ee2161
Sudeep Pillai <sudeep.pillai@gmail.com>
2023-04-30 16:49:49 -0700

    Added end-to-end integration tests for the gRPC server
     - fixed minor re-imports in grpc server
     - added pytest-asyncio to requirements
     - updated docker compose with isolated /tmp/docker mounts

--- nos/experimental/grpc/__init__.py
+++ nos/experimental/grpc/__init__.py
@@ -1,1 +1,6 @@
 from .protoc import import_module  # noqa F401
+from .client import remote_model  # noqa F401
+from .server import InferenceService  # noqa F401
+
+
+DEFAULT_GRPC_PORT = 50051
--- tests/experimental/grpc/test_grpc_client.py
+++ tests/experimental/grpc/test_grpc_client.py
@@ -1,61 +1,51 @@
-"""
-Simple gRPC client for NOS service.
+"""Fully integrated gRPC client test with gRPC-based inference server.
 
-Used for testing purposes and in conjunction with the NOS gRPC server (grpc_server.py).
-"""
-import asyncio
-from contextlib import asynccontextmanager
+The test spins up a gRPC inferennce server and then sends requests to it using the gRPC client.
 
+"""
+import grpc
 import numpy as np
+import pytest
 import ray
 from google.protobuf import empty_pb2
 from PIL import Image
 from tqdm import tqdm
 
-import grpc
-from nos.experimental.grpc import import_module
+from nos.experimental.grpc import DEFAULT_GRPC_PORT, InferenceService, import_module, remote_model
 from nos.test.utils import NOS_TEST_IMAGE
 
+
+GRPC_TEST_PORT = DEFAULT_GRPC_PORT + 1
 
 nos_service_pb2 = import_module("nos_service_pb2")
 nos_service_pb2_grpc = import_module("nos_service_pb2_grpc")
 
 
-@asynccontextmanager
-async def remote_model(model_name: str, address: str = "localhost:50051"):
-    """Remote model context manager."""
-    async with grpc.aio.insecure_channel(address) as channel:
-        # Create inference stub and init model
-        stub = nos_service_pb2_grpc.InferenceServiceStub(channel)
-        request = nos_service_pb2.InitModelRequest(model_name=model_name, min_replicas=1, max_replicas=2)
-        response: nos_service_pb2.InitModelResponse = await stub.InitModel(request)
-        print(response.result)
+def grpc_server(port: int = GRPC_TEST_PORT):
+    from loguru import logger
 
-        # Yield the gprc stub once the model is initialized
-        yield stub
-
-        # Delete model
-        request = nos_service_pb2.DeleteModelRequest(model_name=model_name)
-        response: nos_service_pb2.DeleteModelResponse = await stub.DeleteModel(request)
-        print(response.result)
+    logger.info(f"Starting gRPC test server on port: {port}")
+    server = grpc.aio.server()
+    nos_service_pb2_grpc.add_InferenceServiceServicer_to_server(InferenceService(), server)
+    server.add_insecure_port(f"[::]:{port}")
+    return server
 
 
-def _nos_grpc():
+@pytest.mark.e2e
+@pytest.mark.asyncio
+async def test_e2e_grpc_client_and_server():
     img = Image.open(NOS_TEST_IMAGE)
     img = np.array(img)
 
-    async def send_request():
-        async with grpc.aio.insecure_channel("localhost:50051") as channel:
-            stub = nos_service_pb2_grpc.InferenceServiceStub(channel)
-
-            # List models
-            request = empty_pb2.Empty()
-            response: nos_service_pb2.ModelListResponse = await stub.ListModels(request)
-            print(response.models)
+    async def send_request(stub):
+        # List models
+        request = empty_pb2.Empty()
+        response: nos_service_pb2.ModelListResponse = await stub.ListModels(request)
+        print(response.models)
 
         # TXT2VEC
         method, model_name = "txt2vec", "openai/clip-vit-base-patch32"
-        async with remote_model(model_name=model_name) as stub:
+        async with remote_model(stub, model_name=model_name):
             for _ in tqdm(range(10), desc=f"Bench [method{method}, model_name={model_name}]"):
                 response = await stub.Predict(
                     nos_service_pb2.InferenceRequest(
@@ -68,7 +58,7 @@ def _nos_grpc():
 
         # IMG2VEC
         method, model_name = "img2vec", "openai/clip-vit-base-patch32"
-        async with remote_model(model_name=model_name) as stub:
+        async with remote_model(stub, model_name=model_name):
             for _ in tqdm(range(10), desc=f"Bench [method{method}, model_name={model_name}]"):
                 response = await stub.Predict(
                     nos_service_pb2.InferenceRequest(
@@ -81,7 +71,7 @@ def _nos_grpc():
 
         # TXT2IMG
         method, model_name = "txt2img", "stabilityai/stable-diffusion-2"
-        async with remote_model(model_name=model_name) as stub:
+        async with remote_model(stub, model_name=model_name):
             for _ in tqdm(range(10), desc=f"Bench [method{method}, model_name={model_name}]"):
                 response = await stub.Predict(
                     nos_service_pb2.InferenceRequest(
@@ -92,11 +82,10 @@ def _nos_grpc():
                 )
                 ray.cloudpickle.loads(response.result)
 
-    async def main():
-        await send_request()
-
-    asyncio.run(main())
-
-
-if __name__ == "__main__":
-    _nos_grpc()
+    # Start the gRPC server
+    server = grpc_server(port=GRPC_TEST_PORT)
+    async with grpc.aio.insecure_channel(f"localhost:{GRPC_TEST_PORT}") as channel:
+        stub = nos_service_pb2_grpc.InferenceServiceStub(channel)
+        await server.start()
+        await send_request(stub)
+        await server.stop(grace=None)
