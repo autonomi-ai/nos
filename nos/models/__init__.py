0846ee5e69d36cc6327aeef6a2a7dfae3201ab40
outtanames <scottloftinwastaken@gmail.com>
2023-05-05 13:24:41 -0700

    lint

--- nos/models/__init__.py
+++ nos/models/__init__.py
@@ -3,5 +3,5 @@ import torch
 from nos import hub
 
 from .clip import CLIP  # noqa: F401
+from .mmdetection import MMDetection  # noqa: F401
 from .stable_diffusion import StableDiffusion2  # noqa: F401
-from .mmdetection import MMDetection # noqa: F401
