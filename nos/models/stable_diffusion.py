21853cb0937c9aafbd02598dfc228fb3b3a1dbf2
Sudeep Pillai <sudeep.pillai@gmail.com>
2023-06-07 16:27:18 -0700

    Refactor stable diffusion with more models and ControlNet guides
     - Added new `extras` requirements for server with auxiliary
       models for ControlNet
     - Added tests for controlnet image generation
     - Added demo controlnet demo notebook

--- nos/models/stable_diffusion.py
+++ nos/models/stable_diffusion.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import List, Union
 
 import torch
@@ -6,10 +7,33 @@ from PIL import Image
 from nos import hub
 from nos.common import TaskType
 from nos.common.types import Batch, ImageSpec, ImageT
+from nos.hub import HuggingFaceHubConfig
 
 
-class StableDiffusion2:
+@dataclass(frozen=True)
+class StableDiffusionConfig(HuggingFaceHubConfig):
+    """Configuration for StableDiffusion model."""
+
+    pass
+
+
+class StableDiffusion:
     """StableDiffusion model for text to image generation."""
+
+    configs = {
+        "CompVis/stable-diffusion-v1-4": StableDiffusionConfig(
+            model_name="CompVis/stable-diffusion-v1-4",
+        ),
+        "runwayml/stable-diffusion-v1-5": StableDiffusionConfig(
+            model_name="runwayml/stable-diffusion-v1-5",
+        ),
+        "stabilityai/stable-diffusion-2": StableDiffusionConfig(
+            model_name="stabilityai/stable-diffusion-2",
+        ),
+        "stabilityai/stable-diffusion-2-1": StableDiffusionConfig(
+            model_name="stabilityai/stable-diffusion-2-1",
+        ),
+    }
 
     def __init__(
         self,
@@ -23,25 +47,33 @@ class StableDiffusion2:
             StableDiffusionPipeline,
         )
 
+        try:
+            self.cfg = StableDiffusion.configs[model_name]
+        except KeyError:
+            raise ValueError(f"Invalid model_name: {model_name}, available models: {StableDiffusion.configs.keys()}")
+
         # Only run on CUDA if available
         if torch.cuda.is_available():
             self.device = "cuda"
             self.dtype = dtype
+            self.revision = "fp16"
             torch.backends.cuda.matmul.allow_tf32 = True
         else:
-            raise RuntimeError("No CUDA device available")
+            self.device = "cpu"
+            self.dtype = torch.float32
+            self.revision = None
 
         if scheduler == "ddim":
-            self.scheduler = DDIMScheduler.from_pretrained(model_name, subfolder="scheduler")
+            self.scheduler = DDIMScheduler.from_pretrained(self.cfg.model_name, subfolder="scheduler")
         elif scheduler == "euler-discrete":
-            self.scheduler = EulerDiscreteScheduler.from_pretrained(model_name, subfolder="scheduler")
+            self.scheduler = EulerDiscreteScheduler.from_pretrained(self.cfg.model_name, subfolder="scheduler")
         else:
             raise ValueError(f"Unknown scheduler: {scheduler}, choose from: ['ddim', 'euler-discrete']")
         self.pipe = StableDiffusionPipeline.from_pretrained(
-            model_name,
+            self.cfg.model_name,
             scheduler=self.scheduler,
             torch_dtype=self.dtype,
-            revision="fp16",
+            revision=self.revision,
         )
         self.pipe = self.pipe.to(self.device)
         self.pipe.enable_attention_slicing()
@@ -79,13 +111,14 @@ class StableDiffusion2:
 # def stable_diffusion_2_ddim_fp16():
 #     return StableDiffusion2("stabilityai/stable-diffusion-2", scheduler="ddim", dtype=torch.float16)
 #
-hub.register(
-    "stabilityai/stable-diffusion-2",
-    TaskType.IMAGE_GENERATION,
-    StableDiffusion2,
-    init_args=("stabilityai/stable-diffusion-2",),
-    init_kwargs={"scheduler": "ddim", "dtype": torch.float16},
-    method_name="__call__",
-    inputs={"prompts": Batch[str], "num_images": int, "height": int, "width": int},
-    outputs={"images": Batch[ImageT[Image.Image, ImageSpec(shape=(None, None, 3), dtype="uint8")]]},
-)
+for model_name in StableDiffusion.configs.keys():
+    hub.register(
+        model_name,
+        TaskType.IMAGE_GENERATION,
+        StableDiffusion,
+        init_args=(model_name,),
+        init_kwargs={"scheduler": "ddim", "dtype": torch.float16},
+        method_name="__call__",
+        inputs={"prompts": Batch[str], "num_images": int, "height": int, "width": int},
+        outputs={"images": Batch[ImageT[Image.Image, ImageSpec(shape=(None, None, 3), dtype="uint8")]]},
+    )
--- tests/models/test_stable_diffusion.py
+++ tests/models/test_stable_diffusion.py
@@ -2,37 +2,60 @@ from typing import List
 
 import pytest
 
-from nos.models import StableDiffusion2  # noqa: F401
-from nos.test.utils import PyTestGroup, skip_all_if_no_torch_cuda
+from nos.test.utils import PyTestGroup, skip_if_no_torch_cuda
 
 
-pytestmark = skip_all_if_no_torch_cuda()
+MODEL_NAME = "runwayml/stable-diffusion-v1-5"
 
 
 @pytest.fixture(scope="module")
 def model():
+    from nos.models import StableDiffusion  # noqa: F401
+
     # TODO (spillai): @pytest.parametrize("scheduler", ["ddim", "euler-discrete"])
-    MODEL_NAME = "stabilityai/stable-diffusion-2"
-    yield StableDiffusion2(model_name=MODEL_NAME, scheduler="ddim")
+    yield StableDiffusion(model_name=MODEL_NAME, scheduler="ddim")
 
 
+@pytest.mark.benchmark(group=PyTestGroup.HUB)
 def test_stable_diffusion(model):
+    """Use StableDiffusion to generate an image from a text prompt.
+
+    Note: This test should be able to run with CPU or GPU.
+    CPU: 3.6s/it
+    GPU (2080 Ti): 8.5it/s
+    GPU (4090): 9.8it/s
+    """
     from PIL import Image
 
     images: List[Image.Image] = model.__call__(
         "astronaut on a horse on the moon",
         num_images=1,
-        num_inference_steps=10,
+        num_inference_steps=100,
         guidance_scale=7.5,
+        width=512,
+        height=512,
     )
     (image,) = images
     assert image is not None
-    assert image.size == (768, 768)
+    assert image.size == (512, 512)
 
 
+@pytest.mark.benchmark(group=PyTestGroup.HUB)
+def test_stable_diffusion_download_all():
+    """Download all StableDiffusion models from HuggingFace Hub."""
+    import torch
+    from diffusers import StableDiffusionPipeline
+
+    from nos.models import StableDiffusion  # noqa: F401
+
+    for _, config in StableDiffusion.configs.items():
+        StableDiffusionPipeline.from_pretrained(config.model_name, torch_dtype=torch.float16)
+
+
+@skip_if_no_torch_cuda
 @pytest.mark.benchmark(group=PyTestGroup.BENCHMARK_MODELS)
 def test_stable_diffusion_benchmark(model):
-    """Benchmark StableDiffusion2 model."""
+    """Benchmark StableDiffusion model."""
     from nos.test.benchmark import run_benchmark
 
     steps = 10
@@ -45,4 +68,4 @@ def test_stable_diffusion_benchmark(model):
         ),
         num_iters=5,
     )
-    print(f"BENCHMARK [StableDiffusion2]: {time_ms / steps:.2f} ms / step")
+    print(f"BENCHMARK [{MODEL_NAME}]: {time_ms / steps:.2f} ms / step")
