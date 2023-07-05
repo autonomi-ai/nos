9711bd7aab7ffe18bf18177899755c1870474357
Sudeep Pillai <sudeep.pillai@gmail.com>
2023-07-04 23:53:45 -0700

    Reduce ray object store memory to 2gb
    MacOS has performance issues with ray object store memory > 2gb.
    This can be fixed by setting the environment variable
    `RAY_ENABLE_MAC_LARGE_OBJECT_STORE=1`.

--- nos/executors/ray.py
+++ nos/executors/ray.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 
 NOS_RAY_NS = os.getenv("NOS_RAY_NS", "nos-dev")
 NOS_RAY_RUNTIME_ENV = os.getenv("NOS_RAY_ENV", None)
-NOS_RAY_OBJECT_STORE_MEMORY = int(os.getenv("NOS_RAY_OBJECT_STORE_MEMORY", 4 * 1024 * 1024 * 1024))  # 4GB
+NOS_RAY_OBJECT_STORE_MEMORY = int(os.getenv("NOS_RAY_OBJECT_STORE_MEMORY", 2 * 1024 * 1024 * 1024))  # 2GB
 
 
 @dataclass
