8b4b71272875d49eb20d557b2cc3c3c1371e1128
Sudeep Pillai <sudeep.pillai@gmail.com>
2023-07-06 16:47:56 -0700

    Mac M1 fixes for `nos.init`
    - update minimum memory and cpu core requirements for mac
    - fix docker pull status prints

--- nos/server/__init__.py
+++ nos/server/__init__.py
@@ -21,27 +21,33 @@ def init(
 
     import psutil
 
-    from nos.common.system import get_system_info, has_gpu
+    from nos.common.system import get_system_info, has_docker, has_gpu
     from nos.logging import logger
-    from nos.server.runtime import InferenceServiceRuntime
+    from nos.server.runtime import DockerRuntime, InferenceServiceRuntime
 
     _MIN_NUM_CPUS = 4
     _MIN_MEM_GB = 6
     _MIN_SHMEM_GB = 4
 
     def _check_system_requirements():
-        # For now, we require at least 4 physical CPU cores and 6 GB of free memory
-        sysinfo = get_system_info()
-        num_cpus = sysinfo["cpu"]["cores"]["physical"]
-        mem_gb = sysinfo["memory"]["available"] / 1024**3
+        if not has_docker():
+            raise RuntimeError("Docker not found, please install docker before proceeding.")
 
-        if num_cpus <= _MIN_NUM_CPUS:
+        # For now, we require at least 4 physical CPU cores and 6 GB of free memory
+        cl = DockerRuntime.get()._client
+        num_cpus = cl.info().get("NCPU", psutil.cpu_count(logical=False))
+        mem_avail = (
+            min(cl.info().get("MemTotal", psutil.virtual_memory().total), psutil.virtual_memory().available)
+            / 1024**3
+        )
+        logger.debug(f"Checking system requirements: [num_cpus={num_cpus}, mem_avail={mem_avail:.1f}GB]")
+        if num_cpus < _MIN_NUM_CPUS:
             raise ValueError(
                 f"Insufficient number of physical CPU cores ({num_cpus} cores), at least 4 cores required."
             )
-        if mem_gb <= _MIN_MEM_GB:
+        if mem_avail < _MIN_MEM_GB:
             raise ValueError(
-                f"Insufficient available system memory ({mem_gb:.1}GB), at least 6 GB of free memory required."
+                f"Insufficient available system memory ({mem_avail:.1f}GB), at least 6 GB of free memory required."
             )
 
     # Check system requirements
@@ -75,8 +81,15 @@ def init(
     logger.info(f"Starting inference service: [name={runtime.cfg.name}, runtime={runtime}]")
 
     # Determine number of cpus, system memory before starting container
-    num_cpus = max(_MIN_NUM_CPUS, utilization * psutil.cpu_count(logical=False))
-    mem_limit = max(_MIN_MEM_GB, utilization * math.floor(psutil.virtual_memory().available / 1024**3))
+    # Note (spillai): MacOSX compatibility issue where docker does not have access to
+    # the correct number of physical cores and memory.
+    cl = DockerRuntime.get()._client
+    num_cpus = cl.info().get("NCPU", psutil.cpu_count(logical=False))
+    num_cpus = max(_MIN_NUM_CPUS, utilization * num_cpus)
+    mem_limit = (
+        min(cl.info().get("MemTotal", psutil.virtual_memory().total), psutil.virtual_memory().available) / 1024**3
+    )
+    mem_limit = max(_MIN_MEM_GB, utilization * math.floor(mem_limit))
     logger.debug(f"Starting inference container: [num_cpus={num_cpus}, mem_limit={mem_limit}g]")
 
     # Start container
@@ -134,14 +147,14 @@ def _pull_image(image: str, quiet: bool = False, platform: str = None) -> str:
                 f"docker pull {image}", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True
             )
             status_q = deque(maxlen=25)
-            status_str = "Pulling new server image: {image} (this may take a while)."
-            with rich.status.Status(status_str) as status:
+            status_str = f"Pulling new server image: {image} (this may take a while)."
+            with rich.status.Status("[bold white]" + status_str + "[/bold white]") as status:
                 while proc.stdout.readable():
                     line = proc.stdout.readline()
                     if not line:
                         break
                     status_q.append(line.decode("utf-8").strip())
-                    status.update("[bold yellow]" + f"{status_str}\n\t" + "\n\t".join(status_q) + "[/bold yellow]")
+                    status.update("[bold white]" + f"{status_str}\n\t" + "\n\t".join(status_q) + "[/bold white]")
             proc.wait()
             logger.info(f"Pulled new server image: {image}")
         except (docker.errors.APIError, docker.errors.DockerException) as exc:
