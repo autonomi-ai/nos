4c0056207da99a9d696a4812dbf9da9858def24a
Sudeep Pillai <sudeep.pillai@gmail.com>
2023-07-13 11:24:55 -0700

    Disable benchmark CLI if torch is unavailable

--- nos/cli/cli.py
+++ nos/cli/cli.py
@@ -1,6 +1,11 @@
 import typer
 
-from nos.cli.benchmark import benchmark_cli
+try:
+    import torch
+except ImportError:
+    torch = None
+
+
 from nos.cli.docker import docker_cli
 from nos.cli.hub import hub_cli
 from nos.cli.predict import predict_cli
@@ -9,10 +14,13 @@ from nos.cli.system import system_cli
 
 app_cli = typer.Typer(no_args_is_help=True)
 app_cli.add_typer(hub_cli)
-app_cli.add_typer(benchmark_cli)
 app_cli.add_typer(system_cli)
 app_cli.add_typer(docker_cli)
 app_cli.add_typer(predict_cli)
+
+if torch is not None:
+    from nos.cli.benchmark import benchmark_cli
+    app_cli.add_typer(benchmark_cli)
 
 
 if __name__ == "__main__":
