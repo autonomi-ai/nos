da505ff0cafc5f29574852ad5a956b3d8a4fae62
outtanames <scottloftinwastaken@gmail.com>
2023-08-21 13:21:21 -0700

    Move writeup to docs/demos/

--- nos/experimental/discord/nos_bot.py
+++ nos/experimental/discord/nos_bot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import io
 import os
 
 import discord
@@ -9,18 +10,21 @@ import nos
 from nos.client import InferenceClient, TaskType
 
 
-# Init nos server
+# Init nos server, wait for it to spin up then confirm its healthy:
 nos.init(runtime="gpu")
 nos_client = InferenceClient()
 nos_client.WaitForServer()
-assert nos_client.IsHealthy()
+if not nos_client.IsHealthy():
+    raise RuntimeError("NOS server is not healthy")
 
+# Set permissions for our bot to allow it to read messages:
 intents = discord.Intents.default()
 intents.message_content = True
 
+# Create our bot:
 bot = commands.Bot(command_prefix="$", intents=intents)
 
-
+# Create a callback to read messages and generate images from prompt:
 @bot.command()
 async def generate(ctx, *, prompt):
     response = nos_client.Run(
@@ -33,16 +37,14 @@ async def generate(ctx, *, prompt):
     )
     image = response["images"][0]
 
-    # Bit of a kluge but it seems ctx requires image to be a file that is opened through
-    # the discrord API.
-    tmp_file_path = "image.png"
-    image.save(tmp_file_path)
-    with open(tmp_file_path, "rb") as img_file:
-        await ctx.send(file=discord.File(img_file))
+    image_bytes = io.BytesIO()
+    image.save(image_bytes, format="PNG")
+    image_bytes.seek(0)
 
-    os.remove(tmp_file_path)
+    await ctx.send(file=discord.File(image_bytes, filename="image.png"))
 
 
+# Pull API token out of environment and run the bot:
 bot_token = os.environ.get("BOT_TOKEN")
 if bot_token is None:
     raise Exception("BOT_TOKEN environment variable not set")
