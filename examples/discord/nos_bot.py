679e0afee9abab3e973837f252ac11741c449517
outtanames <scottloftinwastaken@gmail.com>
2023-08-30 12:09:50 -0700

    job to thread

--- examples/discord/nos_bot.py
+++ examples/discord/nos_bot.py
@@ -27,13 +27,24 @@ bot = commands.Bot(command_prefix="$", intents=intents)
 TRAINING_CHANNEL_NAME = "training"
 NOS_TRAINING_DIR = NOS_TMP_DIR / "train"
 
+# Init a dictionary to map job ids to thread ids:
+thread_to_job = {}
+
 # Create a callback to read messages and generate images from prompt:
 @bot.command()
 async def generate(ctx, *, prompt):
-    # pull the channel id so we know which model to run:
+    # Pull the thread id so we know which model to run generation on:
+    if ctx.channel.id not in thread_to_job:
+        await ctx.send("No thread found for this channel, please train a model first!")
+        return
+
+    job_id = thread_to_job.get(ctx.channel.id)
+
+    # TODO (Sudeep/Scott): What is the setup for training given the job id? 
+    model_from_job_id = "custom/" + job_id
     response = nos_client.Run(
         TaskType.IMAGE_GENERATION,
-        "stabilityai/stable-diffusion-2",
+        model_from_job_id,
         prompts=[prompt],
         width=512,
         height=512,
@@ -47,7 +58,7 @@ async def generate(ctx, *, prompt):
 
     await ctx.send(file=discord.File(image_bytes, filename="image.png"))
 
-
+ 
 @bot.command()
 async def train(ctx):
     # check that its in the training channel
@@ -93,6 +104,7 @@ async def train(ctx):
     assert job_id is not None
 
     thread.send(f"Started training job: {job_id}")
+    job_to_thread[job_id] = thread
 
 
 # Pull API token out of environment and run the bot:
