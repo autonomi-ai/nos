860940299a184735efdba2613815dd24a241748b
Sudeep Pillai <sudeep.pillai@gmail.com>
2023-08-30 13:39:04 -0700

    Fix OMP_NUM_THREADS defaults in entrypoint, if not already set

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,9 +2,11 @@
 set -e
 set -x
 
-echo "Starting Ray server with OMP_NUM_THREADS=${OMP_NUM_THREADS}..."
+# Get number of cores
+NCORES=$(nproc --all)
+echo "Starting Ray server with OMP_NUM_THREADS=${OMP_NUM_THREADS:-${NCORES}}..."
 # Get OMP_NUM_THREADS from environment variable, if set otherwise use 1
-OMP_NUM_THREADS=${OMP_NUM_THREADS} ray start --head
+OMP_NUM_THREADS=${OMP_NUM_THREADS:-${NCORES}} ray start --head
 
 echo "Starting NOS server..."
 nos-grpc-server
