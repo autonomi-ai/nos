fee0e83889e5de917ab3da3e65733702a0554f69
outtanames <scottloftinwastaken@gmail.com>
2023-09-14 16:44:27 -0700

    lint

--- examples/slack/bot.py
+++ examples/slack/bot.py
@@ -1,17 +1,20 @@
 import os
+
+from flask import Flask, Response, request
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-from flask import Flask, request, Response
+
 import nos
 from nos.client import InferenceClient, TaskType
 from nos.logging import logger
 
+
 app = Flask(__name__)
 
 # Initialize the Slack WebClient
-client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
+client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
 
-nos.init(runtime='gpu')
+nos.init(runtime="gpu")
 
 nos_client = InferenceClient()
 
@@ -26,29 +29,27 @@ logger.debug("Server is healthy!")
 
 BASE_MODEL = "runwayml/stable-diffusion-v1-5"
 
-@app.route('/slack/events', methods=['POST'])
+
+@app.route("/slack/events", methods=["POST"])
 def command():
-    print('we got an event!')
+    print("we got an event!")
     data = request.json
-    if 'challenge' in data:
-        return Response(data['challenge'], mimetype='application/json')
-    
-    channel = data['event']['channel']
-    text = data['event']['text']  
-    user = data['event']['user']
-    print(f'command: {command}')
+    if "challenge" in data:
+        return Response(data["challenge"], mimetype="application/json")
+
+    channel = data["event"]["channel"]
+    text = data["event"]["text"]
+    user = data["event"]["user"]
+    print(f"command: {command}")
 
     try:
-        response = client.chat_postMessage(
-            channel=channel,
-            text=f"generate request: {text}"
-        )
+        response = client.chat_postMessage(channel=channel, text=f"generate request: {text}")
         nos_response = nos_client.Run(
             task=TaskType.IMAGE_GENERATION,
             model_name=BASE_MODEL,
             prompts=[text],
             width=512,
-            height=512, 
+            height=512,
             num_images=1,
         )
         (image,) = nos_response["images"]
@@ -57,16 +58,17 @@ def command():
         image.save("generated.png")
 
         response = client.files_upload(
-                channels=channel,
-                file="generated.png",  # Replace with the actual path to your image
-                title="Generated Image",
-                initial_comment=f"Here's your generated image, <@{user}>!"
-            )
+            channels=channel,
+            file="generated.png",  # Replace with the actual path to your image
+            title="Generated Image",
+            initial_comment=f"Here's your generated image, <@{user}>!",
+        )
         print(response)
     except SlackApiError as e:
         print(f"Error: {e.response['error']}")
 
     return Response(status=200)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(port=int(os.environ.get("PORT", 3000)))
