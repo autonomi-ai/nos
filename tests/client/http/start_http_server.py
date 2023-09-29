7f73e6c0c5e7c69117574b2565c57c1447031ec9
outtanames <scottloftinwastaken@gmail.com>
2023-09-29 16:17:26 -0700

    Use not.init instead, run locust with ui

--- tests/client/http/start_http_server.py
+++ tests/client/http/start_http_server.py
@@ -1,39 +1,16 @@
 # Create a test app to run load testing against:
 import uvicorn
 from nos.server.http._service import app
-from nos.server import InferenceServiceRuntime
-from nos.logging import logger
+import nos
 from nos.constants import DEFAULT_GRPC_PORT
 
-# Start the runtime from test utils
-GPU_CONTAINER_NAME = "nos-inference-service-runtime-gpu-locust-test"
-runtime = InferenceServiceRuntime(runtime="gpu", name=GPU_CONTAINER_NAME)
+# Init a nos backend for REST API:
+nos.init(runtime='gpu', logging_level='DEBUG')
 
-# Force stop any existing containers
-try:
-    runtime.stop()
-except Exception:
-    logger.info(f"Killing any existing container with name: {GPU_CONTAINER_NAME}")
+client = nos.InferenceClient()
 
-# Start grpc server runtime (GPU)
-container = runtime.start(
-    ports={f"{DEFAULT_GRPC_PORT}/tcp": DEFAULT_GRPC_PORT},
-    environment={
-        "NOS_LOGGING_LEVEL": "DEBUG",
-    },
-)
-assert container is not None
-assert container.id is not None
-status = runtime.get_container_status()
-assert status is not None and status == "running"
+client.WaitForServer()
+assert client.IsHealthy()
 
-"""
-# Tear down
-try:
-    runtime.stop()
-except Exception:
-    logger.info(f"Failed to stop existing container with name: {GPU_CONTAINER_NAME}")
-"""
-
-# Start the Fast API app with uvicorn
-uvicorn.run(app(), host="localhost", port=8001, workers=1, log_level="info")
+# Start the Fast API app with uvicorn:
+uvicorn.run(app(), host="localhost", port=8000, workers=1, log_level="info")
--- tests/locustfile.py
+++ tests/locustfile.py
@@ -6,20 +6,21 @@ from nos.test.utils import NOS_TEST_IMAGE
 
 NOS_TEST_IMAGE = "/home/scott/dev/nos/tests/test_data/test.jpg"
 
+data = {
+    "task": "custom",
+    "model_name": "noop/process-images",
+    "inputs": {
+        "images": Image.open(NOS_TEST_IMAGE),
+    },
+}
+
+encoded_data = encode_dict(data)
+
 class NosRestUser(HttpUser):
     @task
     def embed(self):
-
-        data = {
-            "task": "image_embedding",
-            "model_name": "openai/clip",
-            "inputs": {
-                "images": Image.open(NOS_TEST_IMAGE),
-            },
-        }
-
         response = self.client.post(
             "/infer",
             headers={"Content-Type": "application/json"},
-            json=encode_dict(data),
+            json=encoded_data,
         )
