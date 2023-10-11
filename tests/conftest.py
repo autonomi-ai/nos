944c32891068931434403f929d666658cff16216
Sudeep Pillai <sudeep.pillai@gmail.com>
2023-10-11 10:20:51 -0700

    Revert conftest grpc_client/server fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+from nos.test.conftest import (  # noqa: F401, E402  # noqa: F401, E402
+    grpc_client,
+    grpc_client_cpu,
+    grpc_client_gpu,
+    grpc_client_with_cpu_backend,
+    grpc_client_with_gpu_backend,
+    grpc_server,
+    grpc_server_docker_runtime_cpu,
+    grpc_server_docker_runtime_gpu,
+    local_grpc_client_with_server,
+    ray_executor,
+)
