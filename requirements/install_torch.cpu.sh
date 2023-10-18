985a0f6d4e33003d46012b77bfce0affd7f5b37d
Sudeep Pillai <sudeep.pillai@gmail.com>
2023-10-17 22:36:15 -0700

    Cleaned up notebook and several CLI interfaces for v0.1 release (#419)
    ## Summary
    
    Removed torch dependency from notebooks examples and general cleanup
    
    ## Related issues
    
    #417 
    
    ## Checks
    
    - [ ] `make lint`: I've run `make lint` to lint the changes in this PR.
    - [ ] `make test`: I've made sure the tests (`make test-cpu` or `make
    test`) are passing.
    - Additional tests:
       - [ ] Benchmark tests (when contributing new models)
       - [ ] GPU/HW tests

--- requirements/install_torch.cpu.sh
+++ requirements/install_torch.cpu.sh
@@ -2,6 +2,6 @@
 set -ex
 
 echo "Building CPU version of torch"
-mamba install -yv pytorch torchvision cpuonly -c pytorch;
+mamba install -yv pytorch==2.0.1 torchvision cpuonly -c pytorch;
 echo "Building CPU version of torch done"
 python -c "import torch as t; print(f'torch={t.__version__}, cuda={t.cuda.is_available()}, cudnn={t.backends.cudnn.is_available()}')"
