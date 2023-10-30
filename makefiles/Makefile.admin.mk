6f24f555f7ebc0f9aaf69b6b9760123b717768bc
Scott Loftin <106728726+outtanames@users.noreply.github.com>
2023-10-30 15:57:21 -0700

    create-pypi-release-loose -> create-pypi-release (#455)
    No longer using the old `create-pypi-release`, no need for name
    confusion.
    
    ## Checks
    
    - [x] `make lint`: I've run `make lint` to lint the changes in this PR.
    - [x] `make test`: I've made sure the tests (`make test-cpu` or `make
    test`) are passing.
    - Additional tests:
       - [ ] Benchmark tests (when contributing new models)
       - [ ] GPU/HW tests

--- makefiles/Makefile.admin.mk
+++ makefiles/Makefile.admin.mk
@@ -6,7 +6,7 @@ PYPI_PASSWORD :=
 
 WHL_GREP_PATTERN := .*\$(NOS_VERSION).*\.whl
 
-create-pypi-release-loose-test:
+create-pypi-release-test:
 	@echo "looking for nos whl file..."
 	@for file in dist/*; do \
 		echo "examining file: $$file"; \
@@ -18,7 +18,7 @@ create-pypi-release-loose-test:
 	@echo "Upload completed"
 
 
-create-pypi-release-loose:
+create-pypi-release:
 	@echo "looking for nos whl file..."
 	@for file in dist/*; do \
 		echo "examining file: $$file"; \
@@ -28,12 +28,6 @@ create-pypi-release-loose:
 		fi; \
 	done
 	@echo "Upload completed"
-
-create-pypi-release:  ## package, git tag/release and upload a release to PyP I
-	@echo -n "Are you sure you want to create a PyPI release? [y/N] " && read ans && [ $${ans:-N} = y ]
-	echo "Uploading dist/torch_nos-${NOS_VERSION}-py3-none-any.whl"
-	twine upload dist/torch_nos-${NOS_VERSION}-py3-none-any.whl
-	echo "Successfully created release ${NOS_VERSION}."
 
 create-tag:
 	git tag -a ${NOS_VERSION} -m "Release ${NOS_VERSION}"
