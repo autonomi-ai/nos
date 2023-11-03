c2150982e1720f932989dde50e1458614d65b006
Sudeep Pillai <sudeep.pillai@gmail.com>
2023-11-03 16:54:24 -0700

    Various infra updates  - pin torch version `2.0.1`  - build specific cpu/gpu docker targets  - moved `memray` to dev requirements  - updated transformers to `4.35.0` and moved accelerate to gpu build

--- makefiles/Makefile.agibuild.mk
+++ makefiles/Makefile.agibuild.mk
@@ -3,18 +3,19 @@ AGIPACK_ARGS :=
 
 agi-build-cpu:  # equivalent to agi-build-py38-cpu (target=base-cpu, cpu, test-cpu)
 	agi-pack build ${AGIPACK_ARGS} \
+		--target cpu \
 		-c docker/agibuild.cpu.yaml \
 		-o docker/Dockerfile.cpu \
 		-p 3.8.15 \
 		-b debian:buster-slim \
 		-t '${DOCKER_IMAGE_NAME}:${NOS_VERSION_TAG}-{target}'
-
 	docker tag \
 		${DOCKER_IMAGE_NAME}:${NOS_VERSION_TAG}-cpu \
 		${DOCKER_IMAGE_NAME}:latest-cpu
 
 agi-build-gpu:  # equivalent to agi-build-py38-cu118 (target=base-gpu, gpu, test-gpu)
 	agi-pack build ${AGIPACK_ARGS} \
+		--target gpu \
 		-c docker/agibuild.gpu.yaml \
 		-o docker/Dockerfile.gpu \
 		-p 3.8.15 \
@@ -26,6 +27,7 @@ agi-build-gpu:  # equivalent to agi-build-py38-cu118 (target=base-gpu, gpu, test
 
 agi-build-py38-cpu:
 	agi-pack build ${AGIPACK_ARGS} \
+		--target cpu \
 		-c docker/agibuild.cpu.yaml \
 		-o docker/Dockerfile.py38-cpu \
 		-p 3.8.15 \
@@ -34,6 +36,7 @@ agi-build-py38-cpu:
 
 agi-build-py38-cu118:
 	agi-pack build ${AGIPACK_ARGS} \
+		--target gpu \
 		-c docker/agibuild.gpu.yaml \
 		-o docker/Dockerfile.py38-cu118 \
 		-p 3.8.15 \
@@ -42,6 +45,7 @@ agi-build-py38-cu118:
 
 agi-build-py39-cu118:
 	agi-pack build ${AGIPACK_ARGS} \
+		--target gpu \
 		-c docker/agibuild.gpu.yaml \
 		-o docker/Dockerfile.py39-cu118 \
 		-p 3.9.13 \
@@ -50,6 +54,7 @@ agi-build-py39-cu118:
 
 agi-build-py310-cu118:
 	agi-pack build ${AGIPACK_ARGS} \
+		--target gpu \
 		-c docker/agibuild.gpu.yaml \
 		-o docker/Dockerfile.py310-cu118 \
 		-p 3.10.11 \
@@ -58,6 +63,7 @@ agi-build-py310-cu118:
 
 agi-build-py311-cu118:
 	agi-pack build ${AGIPACK_ARGS} \
+		--target gpu \
 		-c docker/agibuild.gpu.yaml \
 		-o docker/Dockerfile.py310-cu118 \
 		-p 3.11.4 \
