fdc08632f234a1546cde6b19012dd91f9689d721
jiexiong2016 <jiexiong@kth.se>
2024-01-19 09:29:26 +0100

    update and enable cli serve test

--- tests/cli/test_cli_serve.py
+++ tests/cli/test_cli_serve.py
@@ -1,3 +1,4 @@
+import contextlib
 import os
 from pathlib import Path
 from typing import Union
@@ -10,6 +11,7 @@ pytestmark = pytest.mark.cli
 runner = CliRunner()
 
 
+@contextlib.contextmanager
 def path_ctx(path: Union[Path, str]):
     prev_cwd = Path.cwd()
     os.chdir(str(path))
@@ -24,15 +26,32 @@ def test_cli_serve_help():
     assert result.exit_code == 0
 
 
-@pytest.mark.skip
-def test_cli_serve_up():
+# @pytest.mark.skip
+def test_cli_serve():
 
     from nos.cli.cli import app_cli
-    from nos.tests.utils import NOS_TEST_DATA_DIR
+    from nos.test.utils import NOS_TEST_DATA_DIR, NOS_TEST_IMAGE
 
+    config_path = NOS_TEST_DATA_DIR / "hub/custom_model/config.yaml"
     # Move to the test data directory
-    path = NOS_TEST_DATA_DIR / "hub/custom_model/config.yaml"
+    with path_ctx(config_path.parent):
+        serve_up_result = runner.invoke(app_cli, ["serve", "up", "-c", "config.yaml", "-d"])
+        assert serve_up_result.exit_code == 0
 
-    with path_ctx(path.parent):
-        result = runner.invoke(app_cli, ["serve", "-c", "config.yaml", "up"])
-        assert result.exit_code == 0
+    from nos.client import Client
+
+    # Wait for the server to be ready
+    client = Client()
+    client.WaitForServer()
+    assert client.IsHealthy()
+
+    # Check if the model is registered
+    model = client.Module("custom-model")
+    assert model is not None
+
+    with client.UploadFile(NOS_TEST_IMAGE) as remote_path:
+        assert remote_path is not None
+
+    # Tear down the model
+    serve_down_result = runner.invoke(app_cli, ["serve", "down", "--target", "custom_model"])
+    assert serve_down_result.exit_code == 0
