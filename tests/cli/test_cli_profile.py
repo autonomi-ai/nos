0b2811d12eef5313146a182125ea6c0d4df1de27
outtanames <scottloftinwastaken@gmail.com>
2024-01-22 18:21:20 -0800

    Add more test coverage for catalog paths

--- tests/cli/test_cli_profile.py
+++ tests/cli/test_cli_profile.py
@@ -19,6 +19,33 @@ def test_cli_profile_model():
     assert result.exit_code == 0
 
 
+@skip_if_no_torch_cuda
+def test_catalog_path():
+    result = runner.invoke(
+        app_cli, ["profile", "model", "-m", "openai/clip", "--catalog-path", "tests/"]
+    )
+    assert result.exit_code == 0
+    import os
+    assert len(os.listdir("tests/")) > 0
+
+
+@skip_if_no_torch_cuda
+def test_gpu_util():
+    result = runner.invoke(
+        app_cli, ["profile", "model", "-m", "openai/clip", "--catalog-path", "tests/"]
+    )
+    assert result.exit_code == 0
+    import os
+
+    assert os.path.exists("tests/")
+    # load the json manually and check the field is not 'nan':
+    import json
+    # open the first file in the tests/ dir:
+    with open(os.listdir("tests/")[0]) as f:
+        catalog = json.load(f)
+    assert catalog["openai/clip"]["gpu_util"] != "nan"
+
+
 @pytest.mark.benchmark(group=PyTestGroup.MODEL_PROFILE)
 def test_cli_profile_all():
     result = runner.invoke(app_cli, ["profile", "all"])
