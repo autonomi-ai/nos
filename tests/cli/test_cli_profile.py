0bec5a1d10efe042c5444e66f81ff2ebc36c90fd
outtanames <scottloftinwastaken@gmail.com>
2024-01-23 15:11:42 -0800

    fix path for util test

--- tests/cli/test_cli_profile.py
+++ tests/cli/test_cli_profile.py
@@ -3,6 +3,7 @@ from typer.testing import CliRunner
 
 from nos.cli.cli import app_cli
 from nos.test.utils import PyTestGroup, skip_if_no_torch_cuda
+import shutil
 
 
 runner = CliRunner()
@@ -22,28 +23,31 @@ def test_cli_profile_model():
 @skip_if_no_torch_cuda
 def test_catalog_path():
     result = runner.invoke(
-        app_cli, ["profile", "model", "-m", "openai/clip", "--catalog-path", "tests/"]
+        app_cli, ["profile", "model", "-m", "openai/clip", "--catalog-path", "./test-catalog/"]
     )
     assert result.exit_code == 0
     import os
-    assert len(os.listdir("tests/")) > 0
+    assert len(os.listdir("./test-catalog/")) > 0
+    shutil.rmtree("./test-catalog/")
 
 
 @skip_if_no_torch_cuda
 def test_gpu_util():
     result = runner.invoke(
-        app_cli, ["profile", "model", "-m", "openai/clip", "--catalog-path", "tests/"]
+        app_cli, ["profile", "model", "-m", "openai/clip", "--catalog-path", "./test-catalog/"]
     )
     assert result.exit_code == 0
     import os
 
-    assert os.path.exists("tests/")
+    assert os.path.exists("./test-catalog/")
     # load the json manually and check the field is not 'nan':
     import json
     # open the first file in the tests/ dir:
-    with open(os.listdir("tests/")[0]) as f:
+    # open the absolute path:
+    with open(os.path.join("./test-catalog/", os.listdir("./test-catalog/")[0])) as f:
         catalog = json.load(f)
-    assert catalog["openai/clip"]["gpu_util"] != "nan"
+    assert catalog["openai/clip"]["gpu_util"] is not "nan"
+    shutil.rmtree("./test-catalog/")
 
 
 @pytest.mark.benchmark(group=PyTestGroup.MODEL_PROFILE)
