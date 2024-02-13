436a2e31a69d4a1841f0ceebb752e9c551a6431f
Sudeep Pillai <sudeep.pillai@gmail.com>
2024-02-13 14:41:58 -0800

    Support single-container gRPC serving + HTTP proxy
    - Updated entrypoint to support both gRPC and HTTP serving
    - Updated default gRPC host to localhost to avoid ipv6 issues within docker
    - Updated docker-compose files to support single-container gRPC serving + HTTP proxy
    - Updated default `nos.client.Client()` address to use `localhost:50051` for gRPC
      and avoid having to manually set for examples.

--- nos/__init__.py
+++ nos/__init__.py
@@ -6,10 +6,3 @@ from nos.version import __version__  # noqa: F401
 from .client import Client  # noqa: F401
 from .logging import logger  # noqa: F401
 from .server import init, shutdown  # noqa: F401
-
-
-def internal_libs_available():
-    """Check if the internal module is available."""
-    from .common.runtime import is_package_available  # noqa: F401
-
-    return is_package_available("autonomi.nos._internal")
